6f49af3c9582b04cb2b97aa3b5caacc65daf145e
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-13 17:32:20 +0200

    Stars Javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,11 @@ require("@rails/activestorage").start()
 require("channels")
 
 import 'bootstrap';
+import { dynamicRating } from "../plugins/starsInReviewForm";
+
+document.addEventListener('turbolinks:load', () => {
+  dynamicRating();
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
