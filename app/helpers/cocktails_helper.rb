70f006540f5be9ff4afd13682d0e415ff66e6c22
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-14 16:34:00 +0200

    Version Finale mais Pas Ouf

--- app/helpers/cocktails_helper.rb
+++ app/helpers/cocktails_helper.rb
@@ -1,2 +1,8 @@
 module CocktailsHelper
+  def icon_trash(name, size=0.7)
+    html = "<i class='fas fa-#{name}' "
+    html += "style='font-size:#{size}em' "
+    html += "></i>"
+    html.html_safe
+  end
 end
--- app/javascript/plugins/banner.js
+++ app/javascript/plugins/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Let's dive into ...", "Cocktails World !"],
-    typeSpeed: 50,
-    loop: true
-  });
+  if (document.querySelector('#banner-typed-text') !== null) {
+    new Typed('#banner-typed-text', {
+      strings: ["Let's dive into ...", "Cocktails World !"],
+      typeSpeed: 50,
+      loop: true
+    });
+  };
 }
 
 export { loadDynamicBannerText };
