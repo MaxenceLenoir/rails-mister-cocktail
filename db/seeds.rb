5a82826cc8875f34c215c2ee138885450c9c3353
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-08-14 17:08:45 +0200

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Cleaning database..."
+Dose.destroy_all
 Ingredient.destroy_all
 Cocktail.destroy_all
+Review.destroy_all
 
 puts "Creating Ingredients and Cocktails..."
 
